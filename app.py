4e4a6d2271fe07b6519575596448e29fccad9161
Leo Lungu <leo.lungu13@yahoo.com>
2022-12-03 19:46:02 +0000

    Connected HTML form with Flask

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from openpyxl import load_workbook
+from flask import Flask, request, render_template
+
 wb = load_workbook("Invoice.xlsx")
 
 sheet = wb.active
+
 
 
 name = sheet['B19']#B19
@@ -23,5 +26,18 @@ address = sheet['G12'] #G12
 
 date = sheet['G13'] #G13
 
+app = Flask(__name__)  
+print("aaaa")
+@app.route('/invoice', methods = ['POST', 'GET'])
+def invoice():
+    if request.method == 'POST':
+        name2 = request.form.get("name")
+        
+    return render_template('invoice.html')
+ 
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
 
 wb.save("Invoice1.xlsx")
+
