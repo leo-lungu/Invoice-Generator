1b615c71457ffb5e3aa3c890b6d0f64076be51d5
Leo Lungu <leo.lungu13@yahoo.com>
2022-12-04 22:02:29 +0000

    Added date and invoice multiplication for total hours and pay

--- app.py
+++ app.py
@@ -18,6 +18,12 @@ thursday = sheet['H19'] #H19
 friday = sheet['I19'] #I19
 saturday = sheet['J19'] #J19
 
+ordinary = sheet['L19']
+
+totalhours = sheet['O19']
+
+totalpay = sheet['Q19']
+
 hourly = sheet['P19'] #P19
 
 invoicenumber = sheet['G11'] #G11
@@ -31,13 +37,43 @@ print("aaaa")
 @app.route('/invoice', methods = ['POST', 'GET'])
 def invoice():
     if request.method == 'POST':
-        name2 = request.form.get("name")
+        name.value = request.form.get("name")
+
+        trade.value = request.form.get("trade")
+
+        mondayForm = float(request.form.get("monday"))
+        tuesdayForm = float(request.form.get("tuesday"))
+        wednesdayForm = float(request.form.get("wednesday"))
+        thursdayForm = float(request.form.get("thursday"))
+        fridayForm = float(request.form.get("friday"))
+        saturdayForm = float(request.form.get("saturday"))
+
+        monday.value = mondayForm
+        tuesday.value = tuesdayForm
+        wednesday.value = wednesdayForm
+        thursday.value = thursdayForm
+        friday.value = fridayForm
+        saturday.value = saturdayForm
+
+        hourlyForm = float(request.form.get("hourlyrate"))
+
+        hourly.value = hourlyForm
+        invoicenumber.value = request.form.get("invoicenumber")
+
+        address.value = request.form.get("address")
+
+        date.value = request.form.get("date")
         
+        totalhoursForm = mondayForm + tuesdayForm + wednesdayForm + thursdayForm + fridayForm + saturdayForm
+        totalhours.value = totalhoursForm
+        ordinary.value = totalhoursForm
+        totalpay.value = totalhoursForm * hourlyForm
+        print(request.form.get("date"))
+        wb.save("Invoice1.xlsx")
+        exit(0)
+
     return render_template('invoice.html')
  
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
-wb.save("Invoice1.xlsx")
-
